ceb4be2b1501729815cb994eaa234f58f5c23ff5
swcl <swcl309@naver.com>
2023-01-25 23:13:38 +0900

    20230125.2313

--- 01_Prefix_Sum/bj_11660.py
+++ 01_Prefix_Sum/bj_11660.py
@@ -1,25 +1,11 @@
-N,M = map(int,input().split())
-mat = []
-for i in range(N):
-    mat.append(list(map(int,input().split())))
-mys = [[0]*N for i in range(N)]
-mys[0][0] = mat[0][0]
-for i in range(0,N):
-    for j in range(0,N):
-        if j == 0:
-            mys[i][j] = mat[i][j]
-        else:
-            mys[i][j] = mys[i][j-1] + mat[i][j]
-for i in range(0,N):
-    for j in range(0,N):
-        if j == 0:
-            mys[j][i] = mys[j][i]
-        else:
-            mys[j][i] = mys[j-1][i] + mat[j][i]
+import sys
+input = sys.stdin.readline
+N , M = map(int,input().split())
+mat = [list(map(int,input().split())) for i in range(N)]
+mys = [[0]*(N+1) for i in range(N+1)]
+for i in range(1,N+1):
+    for j in range(1,N+1):
+        mys[i][j] = mys[i-1][j] + mys[i][j-1] - mys[i-1][j-1] + mat[i-1][j-1]
 for i in range(M):
-    x1,y1,x2,y2 = map(int,input().split())
-    if x1 == 0 and x2 ==0:
-        print(mys[x2-1][y2-1])
-    elif x1 != 0:
-        print()
-    print(mys[x2-1][y2-1]+ mys[x1-1][y1-1]-mys[x1-2][y2-1]-mys[x2-1][y1-2])
+    x1, y1, x2, y2 = map(int,input().split())
+    print(mys[x2][y2] - mys[x1-1][y2]- mys[x2][y1-1] +mys[x1-1][y1-1])
