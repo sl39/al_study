d56c778bfa5fe59eb9cf464196f9638bc6257f7c
이세울 <swcl309@naver.com>
2023-01-27 09:36:00 +0900

    20230127.0935

--- 01_Prefix_Sum/bj_9527.py
+++ 01_Prefix_Sum/bj_9527.py
@@ -12,20 +12,34 @@ bb = binar(B)
 #bc -ac
 ac = 0
 bc = 0
-cnt = 0
-while len(bb):
-    if bb[0] == '1':
-        cnt +=1
-        bc += 2**(len(bb)-1) *cnt
-        bb = bb[1:]
-    else:
-        bb = bb[1:]
-cnt = 0
-while len(ab):
-    if ab[0] == '1':
-        cnt +=1
-        ac += 2**(len(ab)-1) *cnt
-        ab = ab[1:]
-    else:
-        ab = ab[1:]
-print(bc-ac)
+a_len = len(ab)
+b_len = len(bb)
+bil_list = [0] * b_len
+bil_list[0] = 1
+for i in range(1,b_len):
+    bil_list[i]= bil_list[i-1] + (2**(i-1))*(i+2)
+
+a_cnt = 0
+b_cnt = 0
+a_sum = 0
+b_sum = 0
+for i in range(b_len):
+    if i == b_len -1 and bb[i] == "1":
+        b_sum += b_cnt
+        b_cnt += 1
+    elif bb[i] == "1":
+        b_sum += bil_list[b_len-i-2] + b_cnt*(2**(b_len-i-1))
+        b_cnt += 1
+
+
+for i in range(a_len):
+    if i == a_len -1 and ab[i] == "1":
+        a_sum += a_cnt
+        a_cnt += 1
+    elif ab[i] == "1":
+        a_sum += bil_list[a_len-i-2] + a_cnt*(2**(a_len-i-1))
+        a_cnt += 1
+
+a_sum += a_cnt
+b_sum += b_cnt
+print(b_sum - a_sum)
