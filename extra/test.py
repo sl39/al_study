cacc8008743ae96000ccefb2857bb578296f41e4
이세울 <swcl309@naver.com>
2023-01-31 15:55:13 +0900

    202301311555

--- extra/test.py
+++ extra/test.py
@@ -1,16 +1,20 @@
 class Person:
-    count = 0
-    def __init__(self, name):
-        self.name = name
-        Person.count += 1
-        print(self.count)
-    @classmethod
-    def number_of_popilation(cls):
-        print(cls.count)
+    def __init__(self):
+        self._age = 0
+    @property
+    def age(self):          #getter
+        print("getter 호출!")
+        return self._age
 
-person1 = Person("아이유")
-person2 = Person("이찬혁")
+    @age.setter
+    def age(self,age):      #setter
+        print("setter 호출!")
+        self._age = age
 
-Person.number_of_popilation()
-person1.number_of_popilation()
-person2.number_of_popilation()
+    # age = property(get_age,set_age)
+
+p1 = Person()
+# p1._age = 25 # 이거 안됨
+
+p1.age = 20
+print(p1.age)
