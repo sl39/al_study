13afc876d6c3670d6dcf00854e67f7d916172d56
이세울 <swcl309@naver.com>
2023-02-01 16:06:48 +0900

    202302011606

---
+++ 02_SQT/bj_17298.py
@@ -0,0 +1,13 @@
+import sys
+input_ =sys.stdin.readline
+N = int(input_())
+ls = list(map(int,input_().split()))
+r = [-1] *N
+stack = []
+top = -1
+for i in range(N):
+    
+    
+
+for i in r:
+    print(i, end =  " ")
