b4a8482149ffc47ad847cf4598ab561dc78c545e
이세울 <swcl309@naver.com>
2023-02-08 17:57:15 +0900

    20230208

--- extra/test.py
+++ extra/test.py
@@ -1,37 +1,10 @@
-import sys
-input = sys.stdin.readline
-
 N = int(input())
-# room = [list(input().split()) for _ in range(N)]
-room = [list(input()) for _ in range(N)]
-
-sleep_cnt_col = 0
-sleep_cnt_row = 0
+result = []
 for i in range(N):
-    cnt_col = 0
-    cnt_row = 0
-    for j in range(N):
-        if room[i][j] == '.':
-            cnt_col += 1
-        else:
-            if cnt_col >= 2:
-                sleep_cnt_col += 1
-                cnt_col = 0
-            else:
-                cnt_col = 0
+    result.append(int(input()))
 
-        if room[j][i] == '.':
-            cnt_row += 1
-        else:
-            if cnt_row >= 2:
-                sleep_cnt_row += 1
-                cnt_row = 0
-            else:
-                cnt_row = 0
-        
-    if cnt_row >= 2:
-        sleep_cnt_row += 1
-    if cnt_col >= 2:
-        sleep_cnt_col += 1
+cnt = 0
 
-print(sleep_cnt_col, sleep_cnt_row)
+def stair(cnt,):
+    if cnt == N-1:
+    while cnt == N -1: 
