791dfde5d3470cd49d565aab197d198ec0654f70
swcl <swcl309@naver.com>
2023-02-14 22:24:11 +0900

    20230214

--- 04_twopointer/boj_7512.py
+++ 04_twopointer/boj_7512.py
@@ -1,5 +1,11 @@
-TC = int(input())
+num = 10000001
 
-
-for T in range(1,TC+1):
-    n = int(input())
+plist = [1] * num
+plist[0] = 0
+plist[1] = 0
+prime = []
+for i in range(num):
+    if plist[i]:
+        prime.append(i)
+        for j in range(2*i,num,i):
+            plist[j] = 0
