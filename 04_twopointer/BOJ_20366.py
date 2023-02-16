96a5491f77ca2b69e0b4f75b839ff18ddcb76804
Sewool <swcl309@naver.com>
2023-02-16 17:59:15 +0900

    202302161800

---
+++ 04_twopointer/BOJ_20366.py
@@ -0,0 +1,9 @@
+## do you wanna build the snowman~ 
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+snow = list(map(int,input().split()))
+
+mn1 = 1e9
+mn2 = 1e9
--- 04_twopointer/boj_15961.py
+++ 04_twopointer/boj_15961.py
@@ -1,33 +1,56 @@
+## 초밥 먹고 싶다
+
+
+## 아이디어
+## 처음 k개 만큼 종류의 개수를 결과에 더해준다
+## 그리고 슬라이딩 윈도우를 진행하면서
+## 초밥의 가짓수를 더해주거나 빼준다
+## 만약에 초밥에 쿠폰초밥이 없다고 하면 cnt+= 1
+## 없으면 그냥 cnt 와 최대값을 비교해서 max 값 출력
+## 
+
+
+
+
 import sys
 from collections import deque
 
 input = sys.stdin.readline
 
 N, D, K, C = map(int, input().split())
-numbers = [int(input()) for _ in range(N)]
-numbers += numbers[:K-1]
-counter = [ 0 for _ in range(D+1) ]
 
-window = deque()
-count = 0
-answer = 0
-for k, v in enumerate(numbers):
-    window.append(v)
-    counter[v] += 1
-    if counter[v] == 1:
-        count += 1
+check = [0] * (D+1)
 
-    if k < K - 1:
+sushi = []
+for i in range(N):
+    sushi.append(int(input()))
+
+
+sushi = sushi + sushi[:K-1]    
+
+res = []
+cnt = 0
+mx = 0
+
+stack = deque([])
+for k in range(len(sushi)):
+    if check[sushi[k]] == 0:            ### 아직 먹지 않은 초밥이라면 cnt += 1한다
+        cnt += 1
+    check[sushi[k]] += 1                ### 그리고 먹은 거 더해주고
+    stack.append(sushi[k])              ## 스택? 에 더해준다 생각해보니 큐더라
+
+    if k<K-1:                           ## 먹어야하는 초밥 접시수 바로 전까지 계속 더해준다
         continue
 
-    if counter[C] == 0:
-        answer = max(answer, count+1)
+    if check[C] == 0:                   ## 만약 쿠폰초밥이 가짓수에 안들어 간다면
+        mx = max(mx,cnt+1)              ## max 값과 cnt += 1 을 더해줘서 비교
     else:
-        answer = max(answer, count)
+        mx = max(mx,cnt)                ## 아니면 그냥 비교
 
-    p = window.popleft()
-    counter[p] -= 1
-    if counter[p] == 0:
-        count -= 1
+    a = stack.popleft()                 ## 그리고 하나씩 빼준다
+    if check[a] == 1:                   ## 어떤 초밥의 먹은 접시수가 1개면 cnt -= 1
+        cnt -= 1
 
-print(answer)
+    check[a] -= 1                       ## 그리고 체크에서 -= 1
+
+print(mx)
