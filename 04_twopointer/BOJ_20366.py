e1756c0466218af1435c36f2517a7c4b6ccfdbff
swcl <swcl309@naver.com>
2023-02-17 00:31:24 +0900

    202302171231

--- 04_twopointer/BOJ_20366.py
+++ 04_twopointer/BOJ_20366.py
@@ -5,5 +5,23 @@ input = sys.stdin.readline
 n = int(input())
 snow = list(map(int,input().split()))
 
-mn1 = 1e9
-mn2 = 1e9
+snow.sort()
+i = 0
+mn = 4000000001
+
+for i in range(n-3):
+    for j in range(i+3,n):
+        a1 = snow[i] + snow[j]
+        start = i+1
+        end = j-1
+        while start < end:
+            a2 = snow[start] + snow[end]
+            mn = min(mn,abs(a2-a1))
+            if a2 > a1:
+                end -= 1
+            elif a2 < a1:
+                start += 1
+            else:
+                print(0)
+                exit()
+print(mn)
