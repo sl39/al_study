fdf351b12c6534debeaf4dcfbfa81405168cf122
Sewool <swcl309@naver.com>
2023-02-17 17:57:38 +0900

    202302171800

--- 04_twopointer/BOJ_2293.py
+++ 04_twopointer/BOJ_2293.py
@@ -1,15 +1,26 @@
-n, k = map(int,input().split())
-coins = []
-for i in range(n):
-    coins.append(int(input()))
-coins.sort()
-c = max(coins)
-step = [0] * (k+1+c)
-for i in coins:
-    step[i] += 1
-for i in range(1,k+1):
-    if step[i]:
-        for j in coins:
-            step[i+j] += step[i]
-print(step)
+# n, k = map(int,input().split())
+# coins = []
+# for i in range(n):
+#     coins.append(int(input()))
+# coins.sort()
+# c = max(coins)
+# step = [0] * (k+1+c)
+# for i in range(1,k+c+1):
+#     for j in coins:
+#         if i%j == 0:
+#             step[i] += 1
 
+# print(step)
+
+
+
+TC = int(input())
+for T in range(1,TC+1):
+    n = int(input())
+    print(f"#{T}",end = " ")
+    for i in range(int(n**(1/3))+2):
+        if i**3 == n:
+            print(i)
+            break
+    else:
+        print(-1)
