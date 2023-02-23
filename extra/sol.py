d36eda486ee1037b33525328dea6ca2efdfd7f51
swcl <swcl309@naver.com>
2023-02-24 03:06:45 +0900

    20231

--- extra/sol.py
+++ extra/sol.py
@@ -2,35 +2,72 @@ import sys
 sys.stdin = open("s_input.txt")
 
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
+def room_route(x, y):
+    global cnt, check, start
 
-def dfs(start, m):
-    global cnt
-    x,y = start
+    if rooms[y][x] == N * N:
+        result.append(cnt)
+        return
+
     for k in range(4):
-        nx = dx[k] +x
-        ny = dy[k] +y
-        if 0<= nx < n and 0<= ny <n:
-            if mat[x][y] +1 == mat[nx][ny]:
-                dfs([nx,ny], m+1)
-            else:
-                if m > cnt[1]:
-                    cnt = [[i,j],m]
-                elif m == cnt[1] and mat[i][j] < mat[cnt[0][0]][cnt[0][1]]:
-                    cnt = [[i,j],m]
+        nx = x + dx[k]
+        ny = y + dy[k]
+
+        if 0 <= nx < N and 0 <= ny < N:
+            if rooms[ny][nx] == rooms[y][x] + 1:
+                x = nx
+                y = ny
+
+                cnt += 1
+                check[rooms[ny][nx]] = True
 
 
+                room_route(x, y)
+                cnt -= 1
 
+    else:
+        for idx in range(1+N*N):
+            if check[idx] == False:
+                for i in range(N):
+                    for j in range(N):
+                        if rooms[i][j] == idx:
+                            x, y = j, i
+                            check[rooms[y][x]] = True
+                            result.append(cnt)
+                            cnt = 1
+                            start.append(rooms[y][x])
 
-TC = int(input())
-for T in range(1,TC+1):
-    n = int(input())
-    cnt = [[0,0],0]
-    mat = []
-    for i in range(n):
-        mat.append(list(map(int,input().split())))
-    for i in range(n):
-        for j in range(n):
-            dfs([i,j], 0)
-    print(f"#{T}",mat[cnt[0][0]][cnt[0][1]],cnt[1]+1)
+                            room_route(x, y)
+                            return
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+
+    rooms = [list(map(int, input().split())) for _ in range(N)]
+
+    dx = [0, 0, -1, 1]
+    dy = [1, -1, 0, 0]
+
+    check = [False for _ in range(N*N+1)]
+    cnt = 1
+    result = []
+    start = []
+
+    for i in range(N):
+        for j in range(N):
+            if rooms[i][j] == 1:
+                x, y = j, i
+                break
+    check[1] = True
+    start.append(rooms[y][x])
+    room_route(x, y)
+    mx = 0
+    idx = N*N
+    for i in range(len(start)):
+        if result[i] > mx:
+            idx = start[i]
+            mx = result[i]
+    print(mx)
+    print(idx)
