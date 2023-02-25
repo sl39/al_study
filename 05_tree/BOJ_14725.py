b2c8e9e2c60e4e052b3e59b1dcd096922c946638
swcl <swcl309@naver.com>
2023-02-25 21:46:02 +0900

    20230225

--- 05_tree/BOJ_14725.py
+++ 05_tree/BOJ_14725.py
@@ -1,3 +1,45 @@
+# 블로그 아이디어 차용
+# 딕셔러리 안에 딕셔러리를 계속해서 만들어서 함
+# trie 를 만들어 볼까 하다가 이건 공부를 더 해봐야 겠다는 생각이 더 들었음
+# 함수를 2개를 만드는데 하나는 딕셔러리를 계속해서 넣는것과
+# 다른 하나는 print 하는 함수
+
+
+
+
 N = int(input())
+mat = []
 for i in range(N):
+    mat.append(list(input().split()))
+
+
+# ant 함수를 만듬
+def ant(dic,arr):
+
+    # 마지막 까지 해서 할게 없다면 그냥 리턴
+    if len(arr) == 0:
+        return
     
+    # 아니라면 arr[0] 가 없다면 새로 key를 만들고
+    if arr[0] not in dic:
+        dic[arr[0]] = {}
+    
+    # 계속해서 잘라서 넣어줌
+    ant(dic[arr[0]],arr[1:])
+
+
+dic = {}
+
+# 밑으로 탐색하면서 하나씩 출력해줌
+def pd(dic, depth):
+    for i in sorted(dic):
+        print("--"*depth,end="")
+        print(i)
+        pd(dic[i], depth+1)
+
+
+for i in mat:
+    ant(dic,i[1:])
+
+pd(dic,0)
+
--- 05_tree/BOJ_2213.py
+++ 05_tree/BOJ_2213.py
@@ -1,47 +1,65 @@
-from collections import deque
+# 아이디어
+# 한 노드에 두개의 값을 설정하는데
+# 하나는 그 노드를 방문한 노드와
+# 다른 하나는 방문하지 않는 노드를 만든다
+# 그리고 방문한 노드는 그 이전에 방문하지 않는 값에 현재 가중치를 더해줌
+# 방문하지 않은 노드에는 이전 노드의 2개 값중에서 더 큰 값을 더해줌
+# 길도 마찬가지
+
+
+import sys
+input = sys.stdin.readline
+
+
 
 
 n = int(input())
 arr = [0]
-arr =arr + list(map(int,input().split()))
+arr += list(map(int,input().split()))
+tree = [[] for i in range(n+1)]
+visited = [0] *(n+1)
+value = [[0,0] for i in range(n+1)]
+path = [[[]for i in range(2)] for j in range(n+1)]
 
-mat = [[]for i in range(n+1)]
+
 for i in range(n-1):
     a,b = map(int,input().split())
-    mat[a].append(b)
-    mat[b].append(a)
-visited = [0]*(n+1)
-stack = [1]
-nodes = [[] for i in range(n+1)]
+    tree[a].append(b)
+    tree[b].append(a)
 
-while stack:
-    p = stack.pop()
-    for i in mat[p]:
+
+# 노드 함수
+def indep(node):
+    visited[node] = 1
+    value[node][0] += arr[node]
+    path[node][0].append(node)
+
+
+    for i in tree[node]:
         if not visited[i]:
-            visited[i] = p
-            nodes[p].append(i)
-            stack.append(i)
-visited[1] = 0
-
-leafnode= deque([])
-for i in range(2,n+1):
-    if not nodes[i]:
-        leafnode.append(i)
+            indep(i)
 
 
 
-tree = [[0,0] for i in range(n+1)]
-while leafnode:
-    i = leafnode.popleft()
-    if i == 1:
-        pass
-    else:
-        if not nodes[i]:
-            tree[i] = [arr[i],0]
-            tree[visited[i]][0] = max(tree[i][1] + tree[visited[i]][0]+arr[visited[i]],tree[visited[i]][0]+arr[visited[i]])
-        else:
-            tree[visited[i]][0] = max(tree[i][1] + tree[visited[i]][0]+arr[visited[i]],tree[visited[i]][0]+arr[visited[i]])
-            tree[visited[i]][1] = max(tree[visited[i]][1]+tree[i][1],tree[visited[i]][1]+tree[i][0],tree[visited[i]][1])
-        leafnode.append(visited[i])
-        
-print(tree)
+            # 부모 노드의 방문 값은 자식 노드의 방문하지 않는 값을 더해준다
+            value[node][0] += value[i][1]
+
+            # 부모 노드의 방문하지 않는 값은 자식 노드 둘중에 큰 값을 더해줌
+            value[node][1] += max(value[i][0],value[i][1])
+
+
+            # 길도 마찬가지로 해준다
+            path[node][0] += path[i][1]
+            if value[i][0] > value[i][1]:
+                path[node][1] += path[i][0]
+            else:
+                 path[node][1] += path[i][1]
+    return
+indep(1)
+
+if value[1][0] > value[1][1]:
+    print(value[1][0])
+    print(*sorted(path[1][0]))
+else:
+    print(value[1][1])
+    print(*sorted(path[1][1]))
