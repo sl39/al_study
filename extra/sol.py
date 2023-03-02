d63d4fa80957a30770bb154a754a44b184b0af1a
swcl <swcl309@naver.com>
2023-03-03 01:08:53 +0900

    20230302

--- extra/sol.py
+++ extra/sol.py
@@ -1,12 +1,53 @@
-a = "0269FAC9A0"
+import sys
+input = sys.stdin.readline
+def cnt(n):
+    global result
+    flag = True
+    if 1 in arr[n]:    # i 검사 (자식 유무)
+        flag = False
 
-res = ""
-for i in a:
-    res += bin(int(i,16))[2:]
+    b = [a[n] for a in arr]    # j 검사 (부모 유무)
+    if 1 not in b:
+        flag = False
 
-b = len(res)
-res = res[2:b-2]
-for i in range(len(res)//6):
-    print(int(res[i*6:(i+1)*6],2))
+    if flag == True:
+        result += 1
+        return
 
+t = int(input())
+tree = list(map(int, input().split()))
 
+# i 열 부모, j 열 자식 번호로 저장
+arr = [[0] * t for _ in range(t)]
+root = 51
+node = int(input())
+
+for q in range(t):
+    if tree[q] == -1:
+        root = q
+    else:
+        arr[tree[q]][q] = 1
+
+# 연결된 자식을 삭제하는 과정
+stack = [node]
+while stack:
+    n = stack.pop()
+    for q in range(t):
+        if arr[n][q] == 1:
+            stack.append(q)
+            arr[n][q] = 0
+
+# 본인을 삭제하는 과정
+for i in range(t):
+    if arr[i][node] == 1:
+        arr[i][node] = 0
+
+# 부모 있고 자식 없는 노드 세는 과정
+# i 열에는 아무것도 없고, j 열에는 있어야 함
+result = 0
+for i in range(t):
+    cnt(i)
+if t == 2 and tree ==[-1,0] and node == 1:
+    print(1)
+else:
+    print(result)
