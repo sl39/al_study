61101bb442e551cda14d717f5bf0bc73cdba2f12
swcl <swcl309@naver.com>
2023-03-20 17:52:56 +0900

    20230320

--- extra/BOJ_1937.py
+++ extra/BOJ_1937.py
@@ -1,32 +1,29 @@
-# import sys
-# sys.stdin = open("in.txt")
+import sys
+sys.stdin = open("in.txt")
 
 n = int(input())
 mat = []
 for i in range(n):
     mat.append(list(map(int,input().split())))
 
-visited = [[1]*n for i in range(n)]
+visited = [[-1]*n for i in range(n)]
 
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
-def dfs(x,y,depth):
-
+def dfs(x,y):
+    if visited[x][y] == -1:
+        visited[x][y] = 0
         for i in range(4):
             nx = dx[i] + x
             ny = dy[i] + y
-            if 0<= nx < n and 0<= ny < n and mat[x][y] < mat[nx][ny] and visited[nx][ny] < depth+1:
-                visited[nx][ny] = depth + 1
-                dfs(nx,ny,depth+1)
-        return 
+            if 0<= nx < n and 0<= ny < n and mat[x][y] < mat[nx][ny]:
+                visited[x][y] = max(dfs(nx,ny),visited[x][y])
+    return visited[x][y] + 1
 
 
+ans = 0
 for i in range(n):
     for j in range(n):
-        dfs(i,j,visited[i][j])
-ans =0 
-
-for i in visited:
-    ans = max(ans,max(i))
+        ans = max(ans,dfs(i,j))
 
 print(ans)
