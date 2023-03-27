61c1e746a975be5665df1786ca5755b3cd9c5337
swcl309 <swcl309@naver.com>
2023-03-27 21:45:09 +0900

    20230327

--- 06_backtracking/extra/BOJ_15686.py
+++ 06_backtracking/extra/BOJ_15686.py
@@ -1,9 +1,3 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
-
 n,m = map(int,input().split())
 
 mat = []
@@ -11,13 +5,19 @@ for i in range(n):
     mat.append(list(map(int,input().split())))
 
 BBQ = []
+house = []
 for i in range(n):
     for j in range(n):
         if mat[i][j] == 2:
             BBQ.append((i,j))
+        if mat[i][j] == 1:
+            house.append((i,j))
+
 
 cs = len(BBQ)
 mx = 13*2500
+
+
 def com(i,depth, res):
     global mx
     if depth == m:
@@ -31,28 +31,18 @@ def com(i,depth, res):
 
 
 def distance(res):
-    q = deque(res)
-    count = 0
-    visited = [[0]*n for j in range(n)]
-    for ch in res:
-        a,b =ch
-        visited[a][b] = -1
-    while q:
-        x,y = q.popleft()
-        for k in range(4):
-            nx = x + dx[k]
-            ny = y + dy[k]
-            if 0<= nx < n and 0<= ny < n and not visited[nx][ny]:
-                if visited[x][y] == -1:
-                    visited[nx][ny] = 1
-                else:
-                    visited[nx][ny] = visited[x][y] + 1
-                if mat[nx][ny] == 1:
-                    count += visited[nx][ny]
-                if count > mx:
-                    return 13*2500
-                q.append((nx,ny))
-    return count
+    ans = 0
+    for i in house:
+        nn = n*n
+        x,y = i
+        for j in res:
+            nx,ny = j
+            a = abs(x-nx) + abs(y-ny)
+            nn = min(a,nn)
+        ans += nn
+        if ans > mx:
+            return 13*2500
+    return ans
 
 com(0,0,[])
 
--- extra/test.py
+++ extra/test.py
@@ -1,26 +1,31 @@
-n, m = map(int,input().split())
-arr = list(map(int,input().split()))
-
-
-start = 0
-end = 0
-mys = 0
-cnt = 0
-while mys < m and start < n:
-    mys += arr[start]
-    start += 1
-
-while start < n and start >= end:
-    if mys == m:
-        cnt += 1
-        mys -= arr[end]
-        end += 1
-    if mys < m:
-        mys += arr[start]
-        start += 1
-    if mys > m:
-        mys -= arr[end]
-        end += 1
+TC =int(input())
     
 
-print(cnt)
+def find(depth,cnt,next):
+    global res
+    
+    if n == depth:
+        res = min(res,cnt+mat[next][0])
+        return
+    
+    for i in range(1,n):
+        if mat[next][i] != 0:
+            if arr[i] == 0:
+                arr[i] = 1
+                find(depth+1, cnt+mat[next][i],i)
+                arr[i] = 0
+
+for T in range(1,TC+1):
+    n = int(input())
+    mat = []
+    for i in range(n):
+        mat.append(list(map(int,input().split())))
+    res = 5000
+    arr = [0]* n
+    for j in range(n):
+        arr[j] = 1
+        find(1,mat[0][j],j)
+        arr[j] = 0
+    print(f'#{T}',end=" ")
+    print(res)
+    
