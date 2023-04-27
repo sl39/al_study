a8f0535e9c60999d5e5df60b0e92639b1c634688
swcl309 <swcl309@naver.com>
2023-04-27 17:50:42 +0900

    20230427

--- 11_DP/BOJ_23840.py
+++ 11_DP/BOJ_23840.py
@@ -1,56 +1,67 @@
+import sys
+input =sys.stdin.readline
+
+from heapq import heappop, heappush
+
+
 n,m = map(int,input().split())
-graph =[[1e9]*n for i in range(n)]
+graph =[[]for i in range(n)]
 for i in range(m):
     s,e,c = map(int,input().split())
-    graph[s-1][e-1] = c
-    graph[e-1][s-1] = c
+    graph[s-1].append((e-1,c))
+    graph[e-1].append((s-1,c))
 start, end = map(int,input().split())
 start -= 1
 end -= 1
 p = int(input())
 arr = list(map(int,input().split()))
-for i in range(p):
+arr= [start] + arr + [end]
+for i in range(1,p+1):
     arr[i] -= 1
 
-for i in range(n):
-    for j in range(n):
-        if i==j:
+
+def diks(start):
+    k = [1e9]*(n)
+    k[start] = 0
+    q = []
+    heappush(q,(0,start))
+    while q:
+        cost, node = heappop(q)
+        if k[node] < cost:
             continue
-        for k in range(n):
-            if i == k or j == k:
-                continue
-            graph[j][k] = min(graph[j][k],graph[j][i] + graph[i][k])
+        for i in graph[node]:
+            x,c = i
+            if cost + c < k[x]:
+                k[x] = cost+c
+                heappush(q,(k[x],x))
+    return k
 
 
-import sys
-input = sys.stdin.readline
+gogo = diks(start)
 
-patha = (1<<start) | (1<<end)
+pp = [[0]*(p+2) for i in range(p+2)]
+for i in range(p+2):
+    ls = diks(arr[i])
+    
+    for j in range(p+2):
+        pp[i][j] = ls[arr[j]]
 
-dp = [[1e9]*n for i in range((1<<n))]
+dp = [[1e9]*(p+2) for i in range(1<<(p+2))]
 
-dp[(1<<start)][start] = 0
-
-last = patha
-for i in arr:
-    last = last|(1<<i)
-
-
-
-for i in range((1<<start),1<<n):
-    for j in range(n):
-        if dp[i][j] == 1e9:
+dp[(1<<0)|(1<<(p+1))][0] = 0
+for i in range((1<<0)|(1<<(p+1)),1<<(p+2)):
+    for j in range(0,p+1):
+        if i&(1<<j) == 0:
             continue
-        for k in range(n):
-            if i&(1<<k) or graph[j][k] == 1e9:
-                continue
-            dp[i|(1<<k)][k] = min(dp[i|(1<<k)][k],dp[i][j]+graph[j][k])
 
+        for k in range(p+2):
+            if i&(1>>k) or pp[j][k] == 0:
+                continue
+            dp[i|(1<<k)][k] = min(dp[i|(1<<k)][k] , dp[i][j] + pp[j][k])
 
 mx = 1e9
-for i in range(last,1<<n):
-    if i&(last)== last:
-        mx = min(mx, dp[i][end])
 
+for i in range(1,p+1):
+    mx = min(dp[1<<(p+2)-1][i]+pp[i][p+1],mx) 
 
 print(mx)
